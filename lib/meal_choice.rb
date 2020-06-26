f022577cf0b71450b2e94dca90bd7c4114a1c87a
Jay Oliver <jaykamas@gmail.com>
2020-06-26 04:03:25 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 def meal_choice(veg1, veg2, protien = "tofu")
   puts "What a nutritious meal!"
   tempString = "A plate of #{protien} with #{veg1} and #{veg2}."
+  puts tempString
   tempString
-  
 end
